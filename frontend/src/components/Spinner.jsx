bfe9391d4849758aaccf4bce2d90e5171179dc04
Bogdan Maslovets <bogdanmaslovets@gmail.com>
2022-03-31 18:09:29 +0300

    first commit

---
+++ frontend/src/components/Spinner.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Oval } from 'react-loader-spinner'
+
+const Spinner = ({ message }) => {
+  return (
+    <div className='flex flex-col justify-center items-center w-full h-full'>
+      <Oval color='#ff4b33' height={200} width={200} className='m-5' />
+      <p className='text-lg text-center px-2'>{message}</p>
+    </div>
+  )
+}
+
+export default Spinner
